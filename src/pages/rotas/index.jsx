6e57d59382ff8c487405e419eb3aa914d7a0001b
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-08 10:06:09 -0300

    navegando entre as paginas uanddo link

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default function rotas() {
     return (
         <div>
             <h1>Rotas teste</h1>
+            <ul>
+                <Link href="/rotas/params?id=1233&nome=BIBI">
+                    <li>Params</li>
+                </Link>
+            </ul>
         </div>
     )
 }
